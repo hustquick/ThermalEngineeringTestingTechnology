106561d517a5910874b5839c066e5c271f207dc9
Zhang Cheng <hustquick@163.com>
2022-09-25 08:44:49 +0800

    增加了批计算功能，并对原有的文件重新进行了分类。

--- Exc2-11.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-A_d = 1.5
-T_d = 2
-
-xi = np.sqrt(1/((np.pi/np.log(A_d))**2+1))
-omega_n = 2 * np.pi / (T_d * np.sqrt(1 - xi**2))
-print(f"阻尼比为：{xi:.2f}，系统固有频率为：{omega_n:.2f} Hz")
